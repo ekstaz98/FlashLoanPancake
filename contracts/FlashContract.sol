15a29c2cf19065756b9af259c802b9e4138f1216
stanislav <igroook1z2x3c@mail.ru>
2022-10-25 00:39:02 +0700

    chore: receive

--- contracts/FlashContract.sol
+++ contracts/FlashContract.sol
@@ -12,6 +12,8 @@ contract FlashContract is IPancakeV2Call {
     address private constant WETH = 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd; // bsc testnet
     address private constant PancakeFactory = 0x6725F303b657a9451d8BA641348b6761A6CC7a17; // bsc testnet
 
+    receive() external payable {}
+
     function runSwap(
         address tokenSwap0,
         address tokenSwap1,
